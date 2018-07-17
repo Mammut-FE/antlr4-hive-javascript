18deca3c0f5c971a54a13907d5c2585ff27ad524
lleohao <lleohao@hotmail.com>
2018-07-17 11:02:18 +0800

    chore: update version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,11 @@
 declare namespace Antlr4HiveJavascript {
-  export class HplsqlParser {
-    program(): any;
-  }
-
-  export class HplsqlLexer {}
-
-  export class HplsqlListener {}
-
-  export class HplsqlVisitor {}
+    export class HplsqlParser {
+        program(): any;
+    }
+    
+    export class HplsqlLexer {}
+    
+    export class HplsqlListener {}
+    
+    export class HplsqlVisitor {}
 }
