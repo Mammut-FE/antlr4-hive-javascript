2149e2d05d477eee748c8300ca9dbe218bf3a65e
lleohao <lleohao@hotmail.com>
2018-07-17 10:55:32 +0800

    feat: Add types

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+declare namespace Antlr4HiveJavascript {
+  export class HplsqlParser {
+    program(): any;
+  }
+
+  export class HplsqlLexer {}
+
+  export class HplsqlListener {}
+
+  export class HplsqlVisitor {}
+}
